f5a42844d0494a22b2423a2533e8ade74fbf0a51
Zoryx <15801610913@163.com>
2018-06-01 21:22:32 +0800

    lhm

--- Seed/Assets/Scripts/ChaCtrOne.cs
+++ Seed/Assets/Scripts/ChaCtrOne.cs
@@ -4,32 +4,55 @@ using UnityEngine;
 
 public class ChaCtrOne : MonoBehaviour {
     public float MoveSpeed = 1.0f;
-
+    public Rigidbody target_2;
     private bool Onground = true;
     private Rigidbody rig;
-	// Use this for initialization
-	void Awake () {
+    public float force;
+    bool la;
+    // Use this for initialization
+    void Awake () {
         rig = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if(Input.GetAxis("1PLeft")!=0)
         {
             transform.Translate(Vector3.right * MoveSpeed* Input.GetAxis("1PLeft") * Time.deltaTime, Space.World);
+            
+        }
+        else
+        {
+           rig.velocity = new Vector3(0f,rig.velocity.y, 0f);
         }
         //按A键，跳跃
-        
-        //按B键，拉
-        if(Input.GetKeyDown(KeyCode.Joystick1Button1))
+        if (Input.GetKeyUp(KeyCode.Joystick1Button1) || Input.GetKeyUp(KeyCode.Z))
         {
-            print("2");
+            la = true;
         }
-if (Input.GetKeyDown(KeyCode.Space)||(Input.GetKeyDown(KeyCode.Joystick1Button0))&&Onground)
+            //按B键，拉
+        if (Input.GetKey(KeyCode.Joystick1Button1)|| Input.GetKey(KeyCode.Z))
+        {
+            
+            Vector3 dic = new Vector3();
+            dic = (-target_2.position + transform.position) / Vector3.Distance(target_2.position, transform.position);
+            if (la)
             {
-                print("1");
-            rig.velocity = new Vector3(0, 5, 0);
+                target_2.velocity += dic * force;
+                la = false;
             }
+            if (target_2.velocity.y > 0)
+            {
+                target_2.AddForce(dic * force * 1.5f, ForceMode.Force);
+            }
+        }
+        if ((Input.GetKeyDown(KeyCode.Space)||(Input.GetKeyDown(KeyCode.Joystick1Button0)))&&Onground)
+        {
+               
+            rig.velocity = new Vector3(0, 5, 0);
+        }
+
 	}
     private void FixedUpdate()
     {
--- Seed/Assets/Scripts/ChaCtrTwo.cs
+++ Seed/Assets/Scripts/ChaCtrTwo.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ChaCtrTwo : MonoBehaviour {
     public float MoveSpeed = 1.0f;
     private bool OnFloor = true;
-
+    public Rigidbody target_1;
+    public float force;
+    bool la;
     private Rigidbody rig;
     // Use this for initialization
     void Awake()
@@ -20,15 +22,34 @@ public class ChaCtrTwo : MonoBehaviour {
         {
             transform.Translate(Vector3.right * MoveSpeed * Input.GetAxis("2PLeft") * Time.deltaTime, Space.World);
         }
+        else
+        {
+            rig.velocity = new Vector3(0f, rig.velocity.y, 0f);
+        }
         //按A键，跳跃
         if ((Input.GetKeyDown(KeyCode.Joystick2Button0)||Input.GetKeyDown(KeyCode.J))&&OnFloor)
         {
             rig.velocity = new Vector3(0, -5, 0);
         }
+        if (Input.GetKeyUp(KeyCode.Joystick1Button1) || Input.GetKeyUp(KeyCode.K))
+        {
+            la = true;
+        }
         //按B键，拉
-        if (Input.GetKeyDown(KeyCode.Joystick2Button1))
+        if (Input.GetKey(KeyCode.Joystick1Button1) || Input.GetKey(KeyCode.K))
         {
 
+            Vector3 dic = new Vector3();
+            dic = (-target_1.position + transform.position) / Vector3.Distance(target_1.position, transform.position);
+            if (la)
+            {
+                target_1.velocity += dic * force;
+                la = false;
+            }
+            if (target_1.velocity.y < 0)
+            {
+                target_1.AddForce(dic * force * 1.5f, ForceMode.Force);
+            }
         }
     }
     private void FixedUpdate()
