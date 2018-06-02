4039be25943a78061969dcf7062fa771e04bfa2e
Zoryx <15801610913@163.com>
2018-06-02 10:01:44 +0800

    no message

--- Seed/Assets/rollTobigBall.cs
+++ Seed/Assets/rollTobigBall.cs
@@ -6,6 +6,7 @@ public class rollTobigBall : MonoBehaviour {
     public float speed = 5;
     public float bigspeed = 1;
     public float rollspeed = 40;
+    public int k;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,7 +14,7 @@ public class rollTobigBall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(new Vector3(speed,bigspeed/2, 0) * Time.deltaTime, Space.World);
+        transform.Translate(new Vector3(speed,k*bigspeed/2, 0) * Time.deltaTime, Space.World);
         transform.Rotate(new Vector3(0, 0, 1) * rollspeed*Time.deltaTime);
         transform.localScale += new Vector3(1, 1, 1) * bigspeed * Time.deltaTime;
 	}
